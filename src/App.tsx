326eca4e0d1bb6959f3f5ada6fa943a450da4e11
Kaspar J. Pärtel <kasparjohannes@gmail.com>
2022-09-24 17:05:33 +0300

    added preloader

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,26 @@ import { NavBar } from "./components";
 import { AboutMe, ContactMe, Landing, Skills, Work, Footer } from "./pages";
 
 import styles from "./app.module.css";
+import { useEffect } from "react";
 
 const App = () => {
+  const loader = document.querySelector(".loader") as HTMLElement;
+
+  useEffect(() => {
+    const onPageLoad = () => {
+      loader.style.display = "none";
+    };
+
+    // Check if the page has already loaded
+    if (document.readyState === "complete") {
+      onPageLoad();
+    } else {
+      window.addEventListener("load", onPageLoad);
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener("load", onPageLoad);
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <div className={styles.container}>
--- src/components/ui/NavBar/NavBar.tsx
+++ src/components/ui/NavBar/NavBar.tsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
-// import { HiMenuAlt4, HiOutlineX } from "react-icons/hi";
-import { Squeeze as Hamburger, Squeeze } from "hamburger-react";
+import { Squeeze as Hamburger } from "hamburger-react";
 
 import styles from "./Navbar.module.css";
 
-import { IconContext } from "react-icons";
 import SwitchIcon from "../../../assets/images/switch-off-outline.svg";
 
 interface TopNavProps {
@@ -16,23 +14,13 @@ const TopNav = ({ isNavOpen, setIsNavOpen }: TopNavProps) => {
   return (
     <div className={styles.items}>
       <img className={styles.icon} src={SwitchIcon} alt="Theme switch icon" />
-      <IconContext.Provider
-        value={{
-          size: "1.7em",
-          className: styles.icon,
-        }}
-      >
-        {/* {isNavOpen ? (
-          <HiOutlineX onClick={() => setIsNavOpen(!isNavOpen)} />
-        ) : (
-          <HiMenuAlt4 onClick={() => setIsNavOpen(!isNavOpen)} />
-        )} */}
-        <Squeeze
-          size={32}
+      <div className={styles["icon-hamburger"]}>
+        <Hamburger
+          size={25}
           toggled={isNavOpen}
           toggle={() => setIsNavOpen(!isNavOpen)}
         />
-      </IconContext.Provider>
+      </div>
     </div>
   );
 };
