134a3670f24ca6e446068227c9895f0c3256d364
Kaspar J. Pärtel <kasparjohannes@gmail.com>
2022-09-28 00:17:14 +0300

    working on sending email with emailjs

--- src/pages/ContactMe/ContactMe.tsx
+++ src/pages/ContactMe/ContactMe.tsx
@@ -1,8 +1,9 @@
-import { Fragment } from "react";
+import { Fragment, useRef, useState } from "react";
 import styles from "./ContactMe.module.css";
 
 import emailIcon from "../../assets/images/icon_email.svg";
 import phoneIcon from "../../assets/images/icon_phone.svg";
+import * as React from "react";
 
 interface ContactCardProps {
   img: string;
@@ -18,29 +19,63 @@ const ContactCard = ({ img, imgAlt, text }: ContactCardProps) => {
     </div>
   );
 };
+
 const ContactForm = () => {
+  const form = useRef();
+  // const [form, setForm] = useState<{
+  //   user_name: string;
+  //   user_email: string;
+  //   subject: string;
+  //   message: string;
+  // }>({ user_name: "", user_email: "", subject: "", message: "" });
+
+  // const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+  //   const name = e.currentTarget.name;
+  //   setForm({ ...form, [name]: e.currentTarget.value });
+  // };
+
+  // const handleTextAreaChange = (e: React.FormEvent<HTMLTextAreaElement>) => {
+  //   const name = e.currentTarget.name;
+  //   setForm({ ...form, [name]: e.currentTarget.value });
+  // };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
   return (
     <Fragment>
       <h3>Contact form</h3>
-      <form action="javascript:void(0);" className={styles.form}>
+      <form
+        ref={form}
+        onSubmit={(e) => handleSubmit(e)}
+        className={styles.form}
+      >
         <div className={styles.form_grid_1}>
           <input
             type="text"
-            name="name"
+            name="user_name"
             id="form_name"
             placeholder="Your name*"
+            // onChange={handleInputChange}
+            required
           />
           <input
             type="email"
-            name="email"
+            name="user_email"
             id="form_email"
             placeholder="Your email*"
+            // onChange={handleInputChange}
+            required
           />
           <input
             type="text"
             name="subject"
             id="form_subject"
             placeholder="Subject*"
+            // onChange={handleInputChange}
+            required
           />
         </div>
         <div className={styles.form_grid_2}>
@@ -49,6 +84,8 @@ const ContactForm = () => {
             id="form_message"
             rows={5}
             placeholder="Your message*"
+            // onChange={handleTextAreaChange}
+            required
           ></textarea>
           <button>Send message</button>
         </div>
